01ea8ce8d620958f654179ced825fb0c2d0c0516
Cyber Threat <55312560+Nikul3201@users.noreply.github.com>
2025-07-18 18:00:12 -0400

    Update MainTest.java

--- src/main/java/com/test/MainTest.java
+++ src/main/java/com/test/MainTest.java
@@ -16,6 +16,7 @@ public class MainTest {
     RequestSpecification requestSpecification;
     Response response;
     ValidatableResponse validatableResponse;
+
     @Test
     public void verifyStatusCode() {
 
@@ -33,6 +34,9 @@ public class MainTest {
         // Send GET request and get the response
         Response response = requestSpecification.get();
 
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         response.then().statusCode(200);
         System.out.println("==> Fetched book successfully by ID.");
 
@@ -52,12 +56,12 @@ public class MainTest {
 
         // JSON request body
         String body = """
-            {
-                "name": "A Guide to the Bodhisattva Way of Life",
-                "author": "Santideva",
-                "price": 15.41
-            }
-            """;
+                {
+                    "name": "A Guide to the Bodhisattva Way of Life",
+                    "author": "Santideva",
+                    "price": 15.41
+                }
+                """;
 
 
         RequestSpecification requestSpecification = given()
@@ -66,6 +70,9 @@ public class MainTest {
                 .body(body);
 
         Response response = requestSpecification.post();
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
 
         response.then().statusCode(201);
         System.out.println("==> Book created successfully with valid data.");
@@ -76,7 +83,7 @@ public class MainTest {
     public void updateBook() {
 
         // Base URL
-        RestAssured.baseURI = "http://localhost:8082/books/8";
+        RestAssured.baseURI = "http://localhost:8082/books/7";
 
         // Authentication
         String username = "admin";
@@ -84,12 +91,12 @@ public class MainTest {
 
         // Updated book data
         String updatedBody = """
-        {
-            "name": "Updated Guide to the Bodhisattva Way of Life",
-            "author": "Santideva",
-            "price": 18.99
-        }
-        """;
+                {
+                    "name": "Updated Guide to the Bodhisattva Way of Life",
+                    "author": "Santideva",
+                    "price": 18.99
+                }
+                """;
 
         // PUT request to update the book with ID 1
         RequestSpecification requestSpecification = given()
@@ -100,6 +107,9 @@ public class MainTest {
         Response response = requestSpecification.put();
 
         response.then().statusCode(200);
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
 
         System.out.println("==> Book updated successfully.");
     }
@@ -108,20 +118,22 @@ public class MainTest {
     public void deleteBook() {
 
         // Base URL
-        RestAssured.baseURI = "http://localhost:8082/books/1";
+        RestAssured.baseURI = "http://localhost:8082/books/11";
 
         // Authentication
         String username = "admin";
         String password = "password";
 
-        // DELETE request to delete the book with ID 1
+        // DELETE request to delete the book with ID 4
         RequestSpecification requestSpecification = given()
                 .auth().preemptive().basic(username, password);
 
         Response response = requestSpecification.delete();
 
-        response.then().statusCode(200);
-
+//        response.then().statusCode(200);
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         System.out.println("==> Book deleted successfully.");
     }
 
@@ -139,8 +151,9 @@ public class MainTest {
                 .get("/9999"); // Assuming 9999 does not exist
 
         // Assert status code
-        assertEquals("Expected 404 Not Found for invalid book ID", 404, response.getStatusCode());
+        assertEquals("Expected 404 Not Found for invalid book ID", 404);
 
+        System.out.println("=============================================================");
         // Optional: message validation
         String message = response.getBody().asString();
         System.out.println("==> [Invalid Book ID] Response Message: " + message);
@@ -156,6 +169,9 @@ public class MainTest {
                 .get("/1");
 
         assertEquals("Expected 401 Unauthorized when no credentials are provided", 401, response.getStatusCode());
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         System.out.println("==> [Unauthorized Access] Access denied as expected due to missing credentials.");
     }
 
@@ -165,11 +181,11 @@ public class MainTest {
         RestAssured.baseURI = "http://localhost:8082/books";
 
         String body = """
-        {
-            "author": "Author Only",
-            "price": 10.99
-        }
-        """;
+                {
+                    "author": "Author Only",
+                    "price": 10.99
+                }
+                """;
 
         Response response = given()
                 .auth().preemptive().basic("admin", "password")
@@ -179,6 +195,9 @@ public class MainTest {
 
 //        System.out.println(response.statusCode());
         response.then().statusCode(400); // Validation failure
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
 
         System.out.println("==> Validation failed as expected due to missing 'name'.");
     }
@@ -189,12 +208,12 @@ public class MainTest {
         RestAssured.baseURI = "http://localhost:8082/books";
 
         String updateBody = """
-        {
-            "name": "Ghost Book",
-            "author": "No One",
-            "price": 1.00
-        }
-        """;
+                {
+                    "name": "Ghost Book",
+                    "author": "No One",
+                    "price": 1.00
+                }
+                """;
 
         Response response = given()
                 .auth().preemptive().basic("admin", "password")
@@ -205,6 +224,9 @@ public class MainTest {
 //        System.out.println(response.statusCode());
         response.then().statusCode(500);
 
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         System.out.println("==> Update rejected as expected for non-existent ID.");
     }
 
@@ -219,6 +241,9 @@ public class MainTest {
 //        System.out.println(response.statusCode());
         response.then().statusCode(500);
 
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         System.out.println("==> Delete attempt failed correctly for non-existent book.");
     }
 
@@ -227,12 +252,12 @@ public class MainTest {
         RestAssured.baseURI = "http://localhost:8082/books";
 
         String body = """
-        {
-            "name": "Evil Discount Book",
-            "author": "Bad Input",
-            "price": -10.00
-        }
-        """;
+                {
+                    "name": "Evil Discount Book",
+                    "author": "Bad Input",
+                    "price": -10.00
+                }
+                """;
 
         Response response = given()
                 .auth().preemptive().basic("admin", "password")
@@ -242,6 +267,10 @@ public class MainTest {
 
         response.then().statusCode(400); // assuming validation
 
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
+
         System.out.println("==> API rejected negative price as expected.");
     }
 
@@ -250,13 +279,13 @@ public class MainTest {
         RestAssured.baseURI = "http://localhost:8082/books";
 
         String body = """
-        {
-            "name": "Book with Extra",
-            "author": "John Doe",
-            "price": 15.00,
-            "publisher": "Unexpected Publisher"
-        }
-        """;
+                {
+                    "name": "Book with Extra",
+                    "author": "John Doe",
+                    "price": 15.00,
+                    "publisher": "Unexpected Publisher"
+                }
+                """;
 
         Response response = given()
                 .auth().preemptive().basic("admin", "password")
@@ -266,6 +295,9 @@ public class MainTest {
 
         response.then().statusCode(400);
 
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
         System.out.println("==> API call ignored successfully with additional field 'publisher'.");
     }
 
@@ -275,12 +307,12 @@ public class MainTest {
         RestAssured.baseURI = "http://localhost:8082/books";
 
         String body = """
-        {
-            "name": "Bad Type Book",
-            "author": "Tester",
-            "price": "free"
-        }
-        """;
+                {
+                    "name": "Bad Type Book",
+                    "author": "Tester",
+                    "price": "free"
+                }
+                """;
 
         Response response = given()
                 .auth().preemptive().basic("admin", "password")
@@ -289,6 +321,10 @@ public class MainTest {
                 .post();
 
         response.then().statusCode(400);
+
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
 
         System.out.println("==> API rejected string-type price as expected.");
     }
@@ -304,6 +340,10 @@ public class MainTest {
 
         long time = response.time();
         assertTrue(time < 1000, "Response time exceeded limit!");
+
+        System.out.println("=============================================================");
+        String message = response.getBody().asString();
+        System.out.println(message);
 
         System.out.println("==> Response time: " + time + " ms — within acceptable limit.");
     }
